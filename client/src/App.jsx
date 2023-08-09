6fd829c587e09ebcf6056d38385de068d258a61d
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-08-08 23:37:02 -0300

    Link component && styles completed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import style from "./App.module.css";
+import { links } from "./services/constants.js";
+import Link from "./components/Link/Link";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <header></header>
+
+      <section className={style.links}>
+        <Link
+          url={links.internet.url}
+          icon={links.internet.icon}
+          title={links.internet.title}
+          text={links.internet.text}
+        />
+        <Link
+          url={links.alarmas.url}
+          icon={links.alarmas.icon}
+          title={links.alarmas.title}
+          text={links.alarmas.text}
+        />
+        <Link
+          url={links.camaras.url}
+          icon={links.camaras.icon}
+          title={links.camaras.title}
+          text={links.camaras.text}
+        />
+        <Link
+          url={links.seguridad.url}
+          icon={links.seguridad.icon}
+          title={links.seguridad.title}
+          text={links.seguridad.text}
+        />
+      </section>
+
+      <footer></footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/services/constants.js
@@ -0,0 +1,31 @@
+import internetSvg from "../assets/wifi.svg";
+import securityAlarmSvg from "../assets/security-alarm.svg";
+import securityCamSvg from "../assets/security-cam.svg";
+import shielCheckSvg from "../assets/shield-check.svg";
+
+export const links = {
+  internet: {
+    url: "#",
+    icon: internetSvg,
+    title: "Internet",
+    text: "Max 3 palabras",
+  },
+  alarmas: {
+    url: "#",
+    icon: securityAlarmSvg,
+    title: "Alarmas",
+    text: "Max 3 palabras",
+  },
+  camaras: {
+    url: "#",
+    icon: securityCamSvg,
+    title: "Camaras",
+    text: "Max 3 palabras",
+  },
+  seguridad: {
+    url: "#",
+    icon: shielCheckSvg,
+    title: "Seguridad",
+    text: "Max 3 palabras",
+  },
+};
