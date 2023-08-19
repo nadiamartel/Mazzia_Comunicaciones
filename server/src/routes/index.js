da2952f8d0eb6af01dd9d2b0f7cf68d4d3a5f2e1
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-08-18 22:56:26 -0300

    feat: added new endpoint for allplanes

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -2,10 +2,10 @@ const { Router } = require("express");
 const router = Router();
 const usuarioRouter = require("./usuario");
 // const consultasRouter = require("./consultas");
-// const planesRouter = require("./planes");
+const planesRouter = require("./planes");
 
-router.use("/usuario", usuarioRouter)
+router.use("/usuario", usuarioRouter);
 // router.use("/consultas", consultasRouter);
-// router.use("/planes", planesRouter);
+router.use("/planes", planesRouter);
 
 module.exports = router;
