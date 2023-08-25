00fbc28c08a2a044d01796ff2a9583bad14ffbce
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-08-25 14:50:02 -0300

    feat: get plan by id && get all planes made

--- src/controllers/planesControllers/getAllPlanes.js
+++ src/controllers/planesControllers/getAllPlanes.js
@@ -1,7 +1,11 @@
-const { Planes } = require("../db");
+const { Planes } = require("../../db");
 
 const allPlans = async () => {
   const planes = await Planes.findAll();
+
+  if (planes.length === 0) {
+    return "No existen planes";
+  }
 
   return planes;
 };
--- src/handlers/planesHandlers.js
+++ src/handlers/planesHandlers.js
@@ -1,4 +1,5 @@
-const { allPlans } = require("../controllers/planesController");
+const { allPlans } = require("../controllers/planesControllers/getAllPlanes");
+const getPlanById = require("../controllers/planesControllers/getPlanById");
 
 const createPlan = async (req, res) => {
   try {
@@ -7,7 +8,18 @@ const createPlan = async (req, res) => {
 
 const getPlan = async (req, res) => {
   try {
-  } catch (error) {}
+    const { id } = req.params;
+
+    const planFound = await getPlanById(id);
+
+    if (typeof planFound === "string") {
+      return res.status(404).json({ message: planFound });
+    }
+
+    res.status(200).json(planFound);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 };
 
 const updatePlan = async (req, res) => {
@@ -18,6 +30,10 @@ const updatePlan = async (req, res) => {
 const getAllplans = async (req, res) => {
   try {
     const allPlanes = await allPlans();
+
+    if (typeof allPlanes === "string") {
+      return res.status(404).json({ message: allPlanes });
+    }
 
     res.status(200).json(allPlanes);
   } catch (error) {
