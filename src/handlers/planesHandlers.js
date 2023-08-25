0ceb37540659bd4e44e1d38f9e147643fdb1856f
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-08-25 17:07:23 -0300

    feat: post plan route made

--- src/handlers/planesHandlers.js
+++ src/handlers/planesHandlers.js
@@ -1,9 +1,25 @@
+const { validatePlan } = require("../controllers/planesControllers/planSchema");
 const { allPlans } = require("../controllers/planesControllers/getAllPlanes");
 const getPlanById = require("../controllers/planesControllers/getPlanById");
+const postPlan = require("../controllers/planesControllers/postPlan");
 
 const createPlan = async (req, res) => {
   try {
-  } catch (error) {}
+    const valitation = validatePlan(req.body);
+
+    if (!valitation.success) {
+      return res
+        .status(400)
+        .json({ error: valitation.error.issues[0].message });
+    }
+
+    const { name, price, speed, detail } = valitation.data;
+    const newPlan = await postPlan(name, price, speed, detail);
+
+    res.status(201).json(newPlan);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
 };
 
 const getPlan = async (req, res) => {
