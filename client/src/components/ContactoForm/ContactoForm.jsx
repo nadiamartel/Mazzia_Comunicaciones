b9667a58de00594f166a39632585586cfc264f47
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-09-11 15:45:38 -0300

    feat: consultas connected to server

--- client/src/components/ContactoForm/ContactoForm.jsx
+++ client/src/components/ContactoForm/ContactoForm.jsx
@@ -1,19 +1,71 @@
+import { useState } from "react";
 import s from "./ContactoForm.module.css";
+import postConsulta from "./postConsulta";
 
 const ContactoForm = () => {
+  const [consulta, setConsulta] = useState({
+    name: "",
+    phone: "",
+    email: "",
+    message: "",
+  });
+
+  const handleChangeInput = (e) => {
+    const { name, value } = e.target;
+    setConsulta({
+      ...consulta,
+      [name]: value,
+    });
+  };
+
+  const handleSubmitConsulta = async (e) => {
+    e.preventDefault();
+    const respone = await postConsulta(consulta);
+
+    setConsulta({
+      name: "",
+      phone: "",
+      email: "",
+      message: "",
+    });
+  };
+
   return (
-    <form className={s.form} action="">
+    <form className={s.form} onSubmit={handleSubmitConsulta}>
       <h5>Dejanos tu mensaje</h5>
-      <input type="text" placeholder="Nombre" />
-      <input type="number" placeholder="Numero" />
-      <input type="email" placeholder="Correo electronico" />
+
+      <input
+        type="text"
+        placeholder="Nombre"
+        onChange={handleChangeInput}
+        value={consulta.name}
+        name="name"
+      />
+      <input
+        type="number"
+        placeholder="Numero"
+        onChange={handleChangeInput}
+        value={consulta.phone}
+        name="phone"
+      />
+      <input
+        type="email"
+        placeholder="Correo electronico"
+        onChange={handleChangeInput}
+        value={consulta.email}
+        name="email"
+      />
+
       <textarea
         placeholder="Mensaje"
-        name=""
+        onChange={handleChangeInput}
+        value={consulta.message}
+        name="message"
         id=""
         cols="30"
         rows="10"
       ></textarea>
+
       <button>Enviar</button>
     </form>
   );
