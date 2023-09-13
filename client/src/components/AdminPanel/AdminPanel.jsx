d594a35387f52111e49b23d97fc131e6b8ad6fe5
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-09-12 21:23:56 -0300

    feat: added features

--- client/src/components/AdminPanel/AdminPanel.jsx
+++ client/src/components/AdminPanel/AdminPanel.jsx
@@ -1,24 +1,30 @@
 import { useState } from "react";
 import PlanesData from "../dataComponents/planesData";
 import ConsultasData from "../dataComponents/ConsultasData";
+import s from "./AdminPanel.module.css";
 
 const AdminPanel = () => {
   const [dataToShow, setDataToShow] = useState("perfil");
 
   return (
-    <>
+    <div className={s.container}>
       <nav>
         <ul>
           <li onClick={() => setDataToShow("planes")}>Planes</li>
           <li onClick={() => setDataToShow("consultas")}>Consultas</li>
-          <li onClick={() => setDataToShow("usuarios")}>perfil</li>
+          <li onClick={() => setDataToShow("perfil")}>Perfil</li>
         </ul>
       </nav>
 
-      {dataToShow === "planes" && <PlanesData />}
-      {dataToShow === "consultas" && <ConsultasData />}
-      {dataToShow === "perfil" && <section>Admin Data</section>}
-    </>
+      <section className={s.content}>
+        <h3>Panel de control</h3>
+        <div className={s.data}>
+          {dataToShow === "planes" && <PlanesData />}
+          {dataToShow === "consultas" && <ConsultasData />}
+          {dataToShow === "perfil" && <section>Admin Data</section>}
+        </div>
+      </section>
+    </div>
   );
 };
 
