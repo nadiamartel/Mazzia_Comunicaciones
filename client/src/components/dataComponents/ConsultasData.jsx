d57c710e919cd069a6686bee7ccfa4b10dc1ca62
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-09-14 20:18:41 -0300

    feat: delete button added

--- client/src/components/dataComponents/ConsultasData.jsx
+++ client/src/components/dataComponents/ConsultasData.jsx
@@ -7,24 +7,31 @@ const ConsultasData = () => {
   useEffect(() => {
     fetch("http://localhost:3001/consultas")
       .then((res) => res.json())
-      .then((res) => setConsultas(res))
+      .then((res) => {
+        if (res.message) return;
+        setConsultas(res);
+      })
       .catch((error) => console.log(error));
   }, []);
 
   return (
     <>
-      {consultas?.map((consulta) => {
-        return (
-          <DataCard
-            key={consulta.id}
-            id={consulta.id}
-            name={consulta.name}
-            phone={consulta.phone}
-            email={consulta.email}
-            message={consulta.message}
-          />
-        );
-      })}
+      {consultas.length === 0 ? (
+        <h2> No hay consultas </h2>
+      ) : (
+        consultas?.map((consulta) => {
+          return (
+            <DataCard
+              key={consulta.id}
+              id={consulta.id}
+              name={consulta.name}
+              phone={consulta.phone}
+              email={consulta.email}
+              message={consulta.message}
+            />
+          );
+        })
+      )}
     </>
   );
 };
