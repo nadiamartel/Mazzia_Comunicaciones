77eefc7f0e2e6b122fd0562085e1b1ac21f3e4d1
josejaramillo098@gmail.com <josejaramillo098@gmail.com>
2023-09-15 17:50:32 -0300

    feat: Lazy added to admin components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,21 +5,29 @@ import Home from "./components/Home/Home";
 import InternetView from "./components/InternetView/InternetView";
 import EmpresasView from "./components/EmpresasView/EmpresasView";
 import ContactoView from "./components/ContactoView/ContactoView";
-import Login from "./components/Login/Login";
-import AdminPanel from "./components/AdminPanel/AdminPanel";
+import Loader from "./components/loader/Loader";
+import { Suspense, lazy } from "react";
+
+const Login = lazy(() => import("./components/Login/Login"));
+const AdminPanel = lazy(() => import("./components/AdminPanel/AdminPanel"));
+
+// import Login from "./components/Login/Login";
+// import AdminPanel from "./components/AdminPanel/AdminPanel";
 
 function App() {
   return (
     <>
       <Nav />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/planes" element={<InternetView />} />
-        <Route path="/empresas" element={<EmpresasView />} />
-        <Route path="/contacto" element={<ContactoView />} />
-        <Route path="/admin" element={<AdminPanel/>}/>
-        <Route path="/login" element={<Login />} />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/planes" element={<InternetView />} />
+          <Route path="/empresas" element={<EmpresasView />} />
+          <Route path="/contacto" element={<ContactoView />} />
+          <Route path="/admin" element={<AdminPanel />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Suspense>
       <Footer />
     </>
   );
